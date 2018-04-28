8364a5570862f62b33a6afb1455e47fe9ead6d64
julia sheremet <juliajessicasher@gmail.com>
2018-04-28 11:30:46 -0700

    update api key format

--- src/drsearch2.js
+++ src/drsearch2.js
@@ -2,7 +2,7 @@ class DoctorSearch2 {
 
   apiSymptom(userSymptom){
     let ourRequest = new XMLHttpRequest();
-    ourRequest.open('GET',`https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&user_location=45.5231%2C%20122.6765&skip=0&limit=10&user_key=${process.env.API_Key}`);
+    ourRequest.open('GET',`https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&user_location=45.5231%2C%20122.6765&skip=0&limit=50&user_key=${process.env.API_KEY}`);
     ourRequest.onload = function(){
       console.log(ourRequest);
       let ourData = JSON.parse(ourRequest.responseText);//interpret as json data
