50905c2bcbd17fdddfb0e99457c823d908923423
julia sheremet <juliajessicasher@gmail.com>
2018-04-28 13:21:15 -0700

    add new promise object

--- src/drsearch2.js
+++ src/drsearch2.js
@@ -1,16 +1,21 @@
-class DoctorSearch2 {
+class DoctorSearchTwo {
 
   apiSymptom(userSymptom){
-    let ourRequest = new XMLHttpRequest();
-    ourRequest.open('GET', `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&user_location=45.5231%2C%20122.6765&skip=0&limit=1&user_key=${process.env.API_KEY}`);
-    ourRequest.onload = function(){
-      console.log(ourRequest);
-      let ourData = JSON.parse(ourRequest.responseText);//interpret as json data
-      console.log(ourData.data[0].name);
-    };
-    ourRequest.send();//sends to html
-  }
-
+    let promise = new Promise(function(resolve, reject) {
+      let ourRequest = new XMLHttpRequest();
+      ourRequest.open('GET', `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&user_location=45.5231%2C%20122.6765&skip=0&limit=1&user_key=${process.env.API_KEY}`);
+      ourRequest.send();
+      ourRequest.onload = function(){
+        console.log(OurRequest);
+        if (this.status === 200) {
+          console.log(ourRequest.response);
+          resolve(ourRequest.response);
+        } else {
+          reject(Error(ourRequest.statusText));
+        }
+      }
+  });
+  return promise;
 }
 
-export { DoctorSearch2 };
+export { DoctorSearchTwo };
--- src/main.js
+++ src/main.js
@@ -1,25 +1,26 @@
 // import { Methods } from './methods.js';
-import { DoctorSearch2 } from './drsearch2.js';
+import { DoctorSearchTwo } from './drsearch2.js';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from 'jquery';
 
 //
-// function findSymptom(symptom){
-  function findSymptom(){
-    let symptom = ourData.data[0].profile;
-    console.log(symptom);
-    // $("ul#listOfSymptoms").append(ourData.data[0].profile);
-    let arrayofSymptoms = [];
-    for(let i in symptom){
-      if(i.includes('profile') && symptom[i] !=null){
-      $('ul#listOfSymptoms').append('<li>' + symptom[i] + '</li>');
-      console.log(symptom[i]);
-      arrayofSymptoms.push(symptom[i]);
-    }
-  }
-  return arrayofSymptoms;
+function findSymptom(ourData){
+  let drInfo = '';
+//   function findSymptom(){
+//     let symptom = ourData.data[0].profile;
+//     console.log(symptom);
+$("ul#listOfSymptoms").append(ourData.data[0].profile);
+//     let arrayofSymptoms = [];
+//     for(let i in symptom){
+//       if(i.includes('profile') && symptom[i] !=null){
+//       $('ul#listOfSymptoms').append('<li>' + symptom[i] + '</li>');
+//       console.log(symptom[i]);
+//       arrayofSymptoms.push(symptom[i]);
+//     }
+//   }
+//   return arrayofSymptoms;
 }
 
 
@@ -28,7 +29,7 @@ import $ from 'jquery';
 $(document).ready(function(){
   $("#dr-btn").click(function(event){
     event.preventDefault();
-    let classCaller = new DoctorSearch2();//instance of object
+    let classCaller = new DoctorSearchTwo();
     let userSymptom = $("#symptom").val();
     $("#symptom").val("");
     debugger;
@@ -36,17 +37,11 @@ $(document).ready(function(){
     let promiseSymptom = classCaller.apiSymptom(userSymptom);//instance, method, userinput
     console.log(promiseSymptom);
     // let drData = ourData.data[0];
-    findSymptom(drData);
-
-
-    // promiseSymptom.then(function(drData) {
-    //   // findSymptom(symptom);
-    //   // console.log(findSymptom(drData));
-    //   findSymptom(drData);
-    //   // console.log(findSymptom(drData));
-    //   // console.log(drData);
-    // }, function(Error){
-    //   $(".error-output").html("Sorry there is an Error loading your request!");
-    // });
-  });//closes click function
-});//closes .ready function
+    promiseSymptom.then(function(drData) {
+      let ourData = JSON.parse(drData);
+      findSymptom(ourData);//json API data, user input
+    }, function(Error){
+      $(".error-output").html("Sorry there is an Error loading your request!");
+    });
+  });
+});
